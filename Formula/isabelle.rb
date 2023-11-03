a9fcb5730358d86d61843f36eb3fe18fc06aa81b
Brae Webb <email@braewebb.com>
2023-11-03 16:20:44 +1000

    Isabelle 2023

--- Formula/isabelle.rb
+++ Formula/isabelle.rb
@@ -1,16 +1,16 @@
 class Isabelle < Formula
   desc "A generic proof assistant"
   homepage "https://isabelle.in.tum.de/"
-  version "2022"
+  version "2023"
 
   on_macos do
     url "https://isabelle.in.tum.de/dist/Isabelle#{version}_macos.tar.gz"
-    sha256 "28444505089e1e88da5c659b3e9b68cf7a234fb8ad9fa74345beacb352cf042d"
+    sha256 "d15496d92ac7348dff938702099ef6e2bb976aaed6d4d08fb0bad7159f65d7f4"
   end
 
   on_linux do
     url "https://isabelle.in.tum.de/dist/Isabelle#{version}_linux.tar.gz"
-    sha256 "e7fa6e9d9675d7a56751c00fa81ad0d081e1b6ffbdb0d7f02056853ba77f04c6"
+    sha256 "1a8e190ac0f3e6027bb961b954bacd24e75d30f5f5f06f7d14069da57e7746a8"
   end
 
   def install
